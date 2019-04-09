723b93380f9aebd9f83a2fb8079a7f7bd0cfc861
Tianwen Jiang <twjiang@ir.hit.edu.cn>
2019-04-09 15:46:02 -0400

    speed optimization

--- src/models.py
+++ src/models.py
@@ -245,9 +245,10 @@ class GraphSage(nn.Module):
 		"""
 		lower_layer_nodes = list(nodes_batch)
 		nodes_batch_layers = [(lower_layer_nodes,)]
+		# self.dc.logger.info('get_unique_neighs.')
 		for i in range(self.num_layers):
-			lower_samp_neighs, lower_layer_nodes = self._get_unique_neighs_list(lower_layer_nodes)
-			nodes_batch_layers.insert(0, (lower_layer_nodes, lower_samp_neighs))
+			lower_samp_neighs, lower_layer_nodes_dict, lower_layer_nodes= self._get_unique_neighs_list(lower_layer_nodes)
+			nodes_batch_layers.insert(0, (lower_layer_nodes, lower_samp_neighs, lower_layer_nodes_dict))
 
 		assert len(nodes_batch_layers) == self.num_layers + 1
 
@@ -255,10 +256,12 @@ class GraphSage(nn.Module):
 		for index in range(1, self.num_layers+1):
 			nb = nodes_batch_layers[index][0]
 			pre_neighs = nodes_batch_layers[index-1]
+			# self.dc.logger.info('aggregate_feats.')
 			aggregate_feats = self.aggregate(nb, pre_hidden_embs, pre_neighs)
 			sage_layer = getattr(self, 'sage_layer'+str(index))
 			if index > 1:
 				nb = self._nodes_map(nb, pre_hidden_embs, pre_neighs)
+			# self.dc.logger.info('sage_layer.')
 			cur_hidden_embs = sage_layer(self_feats=pre_hidden_embs[nb],
 										aggregate_feats=aggregate_feats)
 			pre_hidden_embs = cur_hidden_embs
@@ -266,11 +269,10 @@ class GraphSage(nn.Module):
 		return pre_hidden_embs
 
 	def _nodes_map(self, nodes, hidden_embs, neighs):
-		layer_nodes, samp_neighs = neighs
+		layer_nodes, samp_neighs, layer_nodes_dict = neighs
 		assert len(samp_neighs) == len(nodes)
-		index = [layer_nodes.index(x) for x in nodes]
+		index = [layer_nodes_dict[x] for x in nodes]
 		return index
-
 
 	def _get_unique_neighs_list(self, nodes, num_sample=10):
 		_set = set
@@ -281,52 +283,48 @@ class GraphSage(nn.Module):
 		else:
 			samp_neighs = to_neighs
 		samp_neighs = [samp_neigh | set([nodes[i]]) for i, samp_neigh in enumerate(samp_neighs)]
-		unique_nodes_list = list(set.union(*samp_neighs))
-		return samp_neighs, unique_nodes_list
+		_unique_nodes_list = list(set.union(*samp_neighs))
+		i = list(range(len(_unique_nodes_list)))
+		unique_nodes = dict(list(zip(_unique_nodes_list, i)))
+		return samp_neighs, unique_nodes, _unique_nodes_list
 
 	def aggregate(self, nodes, pre_hidden_embs, pre_neighs, num_sample=10):
-		# samp_neighs, unique_nodes_list = self._get_unique_neighs_list(nodes, num_sample)
-		# unique_nodes = {n:i for i,n in enumerate(unique_nodes_list)}
-		# embed_matrix = pre_hidden_embs[torch.LongTensor(unique_nodes_list)]
+		unique_nodes_list, samp_neighs, unique_nodes = pre_neighs
 
-		unique_nodes_list, samp_neighs = pre_neighs
-		unique_nodes = {n:i for i,n in enumerate(unique_nodes_list)}
-
-		# aggregate the neighbors, discard self nodes
 		assert len(nodes) == len(samp_neighs)
 		indicator = [(nodes[i] in samp_neighs[i]) for i in range(len(samp_neighs))]
 		assert (False not in indicator)
 		if not self.gcn:
 			samp_neighs = [(samp_neighs[i]-set([nodes[i]])) for i in range(len(samp_neighs))]
-
-		if len(pre_hidden_embs) == len(unique_nodes_list):
+		# self.dc.logger.info('2')
+		if len(pre_hidden_embs) == len(unique_nodes):
 			embed_matrix = pre_hidden_embs
 		else:
 			embed_matrix = pre_hidden_embs[torch.LongTensor(unique_nodes_list)]
-
+		# self.dc.logger.info('3')
 		mask = torch.zeros(len(samp_neighs), len(unique_nodes))
 		column_indices = [unique_nodes[n] for samp_neigh in samp_neighs for n in samp_neigh]
 		row_indices = [i for i in range(len(samp_neighs)) for j in range(len(samp_neighs[i]))]
 		mask[row_indices, column_indices] = 1
+		# self.dc.logger.info('4')
 
-		num_neigh = mask.sum(1, keepdim=True)
-		# print(mask)
-		indexs = [x.nonzero() for x in mask==1]
-		aggregate_feats = []
-		for feat in [embed_matrix[x.squeeze()] for x in indexs]:
-			if len(feat.size()) == 1:
-				aggregate_feats.append(feat.view(1, -1))
-			else:
-				if self.agg_func == 'MEAN':
-					aggregate_feats.append(torch.mean(feat,0).view(1, -1))
-				elif self.agg_func == 'MAX':
+		if self.agg_func == 'MEAN':
+			num_neigh = mask.sum(1, keepdim=True)
+			mask = mask.div(num_neigh).to(embed_matrix.device)
+			aggregate_feats = mask.mm(embed_matrix)
+
+		elif self.agg_func == 'MAX':
+			# print(mask)
+			indexs = [x.nonzero() for x in mask==1]
+			aggregate_feats = []
+			# self.dc.logger.info('5')
+			for feat in [embed_matrix[x.squeeze()] for x in indexs]:
+				if len(feat.size()) == 1:
+					aggregate_feats.append(feat.view(1, -1))
+				else:
 					aggregate_feats.append(torch.max(feat,0)[0].view(1, -1))
-		aggregate_feats = torch.cat(aggregate_feats, 0)
+			aggregate_feats = torch.cat(aggregate_feats, 0)
 
-		# mask = mask.div(num_neigh).to(self.device)
-		# aggregate_feats = mask.mm(embed_matrix)
-		# print(aggregate_feats.size())
-		# assert len(outputs) == len(aggregate_feats)
-		# for i, output in enumerate(outputs):
-		# 	print(aggregate_feats[i] - output)
+		# self.dc.logger.info('6')
+		
 		return aggregate_feats
